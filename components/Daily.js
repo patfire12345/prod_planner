0c1dbfbf08ea848d40f3c1c3624821b423e9a991
Lisa Korolyov <ekoro061@uottawa.ca>
2021-11-23 11:25:44 -0500

    Date format changed in daily layout

--- components/Daily.js
+++ components/Daily.js
@@ -9,11 +9,16 @@ import {
 } from 'react-native'
 import Task from './Task'
 
+const month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const day = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 const Daily = (props) => {
   return (
     <View>
       <Text style={styles.sectionTitle}>Today's Tasks</Text>
-      <Text style={styles.sectionTitle}>{Date()}</Text>
+      <Text
+        style={
+          styles.sectionTitle
+        }>{`${day[new Date().getDay()]}, ${new Date().getDate()} ${month[new Date().getMonth()]} ${new Date().getFullYear()}`}</Text>
 
       <View style={styles.items}>
         {props.taskList.map((task, index) => (
